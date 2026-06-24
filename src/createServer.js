d598a76fda2de1aae87ae0ece4d62e46f8e95578
Sidach Ruslan <flashangel4600@gmail.com>
2026-06-24 10:02:55 +0300

    add fix

--- src/createServer.js
+++ src/createServer.js
@@ -4,47 +4,11 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const hint = 'Please specify a file path after /file/.';
+
 function createServer() {
   return http.createServer((req, res) => {
-    if ((req.url || '/').includes('..')) {
-      res.setHeader('Content-Type', 'text/plain');
-      res.statusCode = 400;
-      res.end('URL is not correct');
-
-      return;
-    }
-
-    const url = new URL(
-      req.url || '/',
-      `http://${req.headers.host || 'localhost'}`,
-    );
-    const pathname = url.pathname;
-
-    if (!pathname.startsWith('/file/')) {
-      res.setHeader('Content-Type', 'text/plain');
-
-      if (path.extname(pathname)) {
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'text/plain');
-
-        res.end(
-          // eslint-disable-next-line max-len
-          'The URL should be something like "/file/nonexistent.txt" or "/file/"',
-        );
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-
-        res.end(
-          // eslint-disable-next-line max-len
-          'The URL should be something like "/file/nonexistent.txt" or "/file/"',
-        );
-      }
-
-      return;
-    }
-
-    if (pathname.includes('//')) {
+    if (req.url.includes('//')) {
       res.setHeader('Content-Type', 'text/plain');
       res.statusCode = 404;
       res.end('URL is not correct');
@@ -52,7 +16,38 @@ function createServer() {
       return;
     }
 
-    const filePath = pathname.slice('/file/'.length) || '/';
+    const url = new URL(req.url || '/', `http://${req.headers.host}`);
+    const { pathname } = url;
+
+    if (!pathname.startsWith('/file')) {
+      res.statusCode = 400;
+      res.setHeader('Content-type', 'text/plain');
+      res.end(hint);
+
+      return;
+    }
+
+    if (pathname === '/file') {
+      res.statusCode = 200;
+      res.setHeader('Content-type', 'text/plain');
+      res.end(hint);
+
+      return;
+    }
+
+    const relativePath = pathname.replace(/^\/file\/?/, '');
+    const filePath = relativePath || 'index.html';
+    const finalPath = path.join(__dirname, '../public', filePath);
+    const publicDir = path.resolve(__dirname, '../public');
+
+    const ext = path.extname(filePath);
+
+    if (!finalPath.startsWith(publicDir)) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Bad request');
+    }
 
     const contentTypes = {
       '.html': 'text/html',
@@ -62,23 +57,7 @@ function createServer() {
       '.json': 'application/json',
     };
 
-    const ext = path.extname(filePath);
-
-    if (!ext && filePath.at(-1) !== '/') {
-      res.setHeader('Content-Type', 'text/plain');
-      res.statusCode = 200;
-
-      res.end('The URL should be something like "/dir/file.txt" or "/file/"');
-
-      return;
-    }
-
-    const finalPathname =
-      filePath.at(-1) !== '/'
-        ? path.join(__dirname, '..', 'public', filePath)
-        : path.join(__dirname, '..', 'public', filePath, 'index.html');
-
-    if (!fs.existsSync(finalPathname)) {
+    if (!fs.existsSync(finalPath)) {
       res.setHeader('Content-Type', 'text/plain');
       res.statusCode = 404;
 
@@ -87,19 +66,16 @@ function createServer() {
       return;
     }
 
-    res.setHeader(
-      'Content-Type',
-      contentTypes[path.extname(finalPathname)] || 'text/plain',
-    );
+    res.setHeader('Content-Type', contentTypes[ext] || 'text/plain');
     res.statusCode = 200;
 
-    const fileStream = fs.createReadStream(finalPathname);
+    const fileStream = fs.createReadStream(finalPath);
 
     fileStream.pipe(res);
 
     fileStream.on('error', () => {
-      res.statusCode = 500;
-      res.end('Server error');
+      res.statusCode = 404;
+      res.end('Not found');
     });
 
     res.on('close', () => {
